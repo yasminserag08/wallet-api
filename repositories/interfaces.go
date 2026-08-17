0e613b0328d1c051e2b13bdb880d1c54488fc02d
Yasmin Serag <159052828+yasminserag08@users.noreply.github.com>
2026-08-17 21:11:05 +0300

    Add wallet repo interface

--- repositories/interfaces.go
+++ repositories/interfaces.go
@@ -1,10 +1,21 @@
 package repositories
 
-import "wallet-api/models"
+import (
+	"wallet-api/models"
+
+	"gorm.io/gorm"
+)
 
 // Used for authentication handlers
 type UserRepositoryInterface interface {
 	CreateUser(user models.User) (models.User, error)
 	GetUserByUsername(username string) (models.User, error)
 	GetUserByID(id uint) (models.User, error)
+}
+
+type WalletRepositoryInterface interface {
+	Create(userID uint) error
+	GetByUserID(userID uint) (models.Wallet, error)
+	UpdateBalance(tx *gorm.DB, walletID uint, newBalance int) error
+	GetByUserIDForUpdate(tx *gorm.DB, userID uint) (models.Wallet, error)
 }
